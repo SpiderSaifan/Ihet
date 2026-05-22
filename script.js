2a050d92e541164790e5e218dc5f25e8e0f2f1e1
SpiderSaifan <amreen.manha1@gmail.com>
2026-05-22 19:34:58 +0600

    First Prototype

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const alphabetImg = document.getElementById("alphabet-img");
+const popup = document.getElementById("img-popup");
 
+alphabetImg.addEventListener("click", () => {
+    popup.style.display = "flex";
+});
+
+popup.addEventListener("click", () => {
+    popup.style.display = "none";
+});
